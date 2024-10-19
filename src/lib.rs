5f7c11d72d77f37b6c8840a5ba5c4db8f0f9e0dd
Brandon Ros <brandonros1@gmail.com>
2024-10-19 14:51:27 -0400

    json

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ use std::str::FromStr;
 
 use async_io::Async;
 use async_tls::TlsConnector;
-use http::{HeaderMap, HeaderName, HeaderValue, Request, Response, StatusCode, Version};
+use http::{HeaderMap, HeaderName, HeaderValue, Request, Response, StatusCode, Uri, Version};
 use futures_lite::io::{AsyncBufReadExt, AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt, BufReader};
 use simple_error::{box_err, SimpleResult};
 
@@ -17,68 +17,6 @@ impl<T: AsyncRead + AsyncWrite + Send + Sync + Unpin> AsyncConnection for T {}
 pub struct HttpClient;
 
 impl HttpClient {
-    pub async fn connect(request: &Request<RequestBody>) -> SimpleResult<Box<dyn AsyncConnection>> {
-        log::debug!("request = {request:?}");
-
-        // Extract the scheme, host, and port from the request
-        let (scheme, host, port) = Self::extract_host_from_request(request)?;
-        let addr = format!("{host}:{port}")
-            .to_socket_addrs()?
-            .next()
-            .ok_or("Failed to resolve host")?;
-        let stream = Async::<std::net::TcpStream>::connect(addr).await?;
-
-        // Optionally add TLS based on the scheme
-        let stream: Box<dyn AsyncConnection> = if scheme == "https" || scheme == "wss" {
-            let tls_connector = TlsConnector::new();
-            Box::new(tls_connector.connect(&host, stream).await?)
-        } else {
-            Box::new(stream)
-        };
-
-        Ok(stream)
-    }
-
-    // Public method to send an HTTP request and return the HTTP response
-    pub async fn send(stream: &mut Box<dyn AsyncConnection>, request: &Request<RequestBody>) -> SimpleResult<Response<ResponseBody>> {
-        // Write the HTTP request to the stream
-        let serialized_request = Self::serialize_http_request(request)?;
-        log::debug!("serialized_request = {serialized_request}");
-        stream.write_all(serialized_request.as_bytes()).await?;
-        stream.flush().await?;
-
-        // Write request body if there is one
-        if request.body().len() > 0 {
-            stream.write_all(request.body()).await?;
-            stream.flush().await?;
-        }
-
-        // Read and parse the response
-        let mut reader = BufReader::new(stream);
-        let response_status_line = Self::read_response_status_line(&mut reader).await?;
-        log::debug!("response_status_line = {response_status_line}");
-        let (response_version, response_status) = Self::parse_response_status_line(&response_status_line)?;
-        let response_headers = Self::read_response_headers(&mut reader).await?;
-        log::debug!("response_headers = {response_headers:?}");
-        let response_body = Self::read_response_body(&mut reader, &response_headers).await?;
-        log::debug!("response_body = {response_body:02x?}");
-
-        // Convert to HTTP crate response
-        let mut response: Response<ResponseBody> = Response::builder()
-            .status(response_status)
-            .version(response_version)
-            .body(response_body)?;
-
-        // Copy response headers to response
-        *response.headers_mut() = response_headers;
-
-        // log
-        log::debug!("response = {response:?}");
-
-        // return
-        Ok(response)
-    }
-
     // Extracts the scheme, host, and port from the request URI
     fn extract_host_from_request(req: &Request<RequestBody>) -> SimpleResult<(String, String, u16)> {
         let uri = req.uri();
@@ -241,5 +179,96 @@ impl HttpClient {
             let num_bytes_read = reader.read_to_end(&mut response_body).await?;
             Ok(response_body[0..num_bytes_read].to_vec())
         }
+    }
+
+    pub async fn connect(request: &Request<RequestBody>) -> SimpleResult<Box<dyn AsyncConnection>> {
+        log::debug!("request = {request:?}");
+
+        // Extract the scheme, host, and port from the request
+        let (scheme, host, port) = Self::extract_host_from_request(request)?;
+        let addr = format!("{host}:{port}")
+            .to_socket_addrs()?
+            .next()
+            .ok_or("Failed to resolve host")?;
+        let stream = Async::<std::net::TcpStream>::connect(addr).await?;
+
+        // Optionally add TLS based on the scheme
+        let stream: Box<dyn AsyncConnection> = if scheme == "https" || scheme == "wss" {
+            let tls_connector = TlsConnector::new();
+            Box::new(tls_connector.connect(&host, stream).await?)
+        } else {
+            Box::new(stream)
+        };
+
+        Ok(stream)
+    }
+
+    // Public method to send an HTTP request and return the HTTP response
+    pub async fn send(stream: &mut Box<dyn AsyncConnection>, request: &Request<RequestBody>) -> SimpleResult<Response<ResponseBody>> {
+        // Write the HTTP request to the stream
+        let serialized_request = Self::serialize_http_request(request)?;
+        log::debug!("serialized_request = {serialized_request}");
+        stream.write_all(serialized_request.as_bytes()).await?;
+        stream.flush().await?;
+
+        // Write request body if there is one
+        if request.body().len() > 0 {
+            stream.write_all(request.body()).await?;
+            stream.flush().await?;
+        }
+
+        // Read and parse the response
+        let mut reader = BufReader::new(stream);
+        let response_status_line = Self::read_response_status_line(&mut reader).await?;
+        log::debug!("response_status_line = {response_status_line}");
+        let (response_version, response_status) = Self::parse_response_status_line(&response_status_line)?;
+        let response_headers = Self::read_response_headers(&mut reader).await?;
+        log::debug!("response_headers = {response_headers:?}");
+        let response_body = Self::read_response_body(&mut reader, &response_headers).await?;
+        log::debug!("response_body = {response_body:02x?}");
+
+        // Convert to HTTP crate response
+        let mut response: Response<ResponseBody> = Response::builder()
+            .status(response_status)
+            .version(response_version)
+            .body(response_body)?;
+
+        // Copy response headers to response
+        *response.headers_mut() = response_headers;
+
+        // log
+        log::debug!("response = {response:?}");
+
+        // return
+        Ok(response)
+    }
+
+    pub async fn json_request<RequestBody, ResponseBody>(url: &str, request_body: &RequestBody) -> SimpleResult<ResponseBody>
+    where 
+        RequestBody: miniserde::Serialize, 
+        ResponseBody: miniserde::Deserialize
+    {
+        // build request
+        let uri: Uri = url.parse()?;
+        let stringified_request_body = miniserde::json::to_string(&request_body);
+        let request_body_bytes = stringified_request_body.as_bytes().to_vec();
+        let request = Request::builder()
+            .method("POST")
+            .uri(uri)
+            .header("Content-Type", "application/json")
+            .header("Content-Length", format!("{}", stringified_request_body.len()))
+            .body(request_body_bytes)?;
+
+        // make request
+        let mut stream = Self::connect(&request).await?;
+        let response = Self::send(&mut stream, &request).await?;
+
+        // parse response
+        let response_body_bytes = response.body().to_owned();
+        let stringified_response_body = String::from_utf8(response_body_bytes)?;
+        let response_body: ResponseBody = miniserde::json::from_str(&stringified_response_body)?;
+
+        // return
+        Ok(response_body)
     }
 }
