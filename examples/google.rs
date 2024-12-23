b8c966a3469ee76a4ccb78ca8c0364fe4b016404
Brandon Ros <brandonros1@gmail.com>
2024-12-22 20:39:59 -0500

    "smol" example

--- examples/google.rs
+++ examples/google.rs
@@ -1,29 +1,34 @@
-use futures_lite::future;
-use http::{Request, Uri};
-use http_client::HttpClient;
+use std::sync::Arc;
 
-fn main() {
-    future::block_on(async {
-        // init logging
-        env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("debug")).init();
+use http::{Method, Request, Response, StatusCode, Version};
+use async_executor::Executor;
+use simple_error::SimpleResult;
+use smol::MainExecutor;
 
-        // Build the URI for the request
-        let uri: Uri = "https://www.google.com/".parse().expect("Failed to parse URI");
+async fn async_main(executor: Arc<Executor<'static>>) -> SimpleResult<()> {
+    // init logging
+    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("debug")).init();
 
-        // Build the GET request
-        let request = Request::builder()
-            .method("GET")
-            .uri(uri)
-            .header("User-Agent", "http_client/1.0")
-            .header("Host", "www.google.com")
-            .body(vec![])
-            .expect("Failed to build request");
+    // Build the URI for the request
+    let uri: Uri = "https://www.google.com/".parse().expect("Failed to parse URI");
 
-        // Get the response
-        let mut stream = HttpClient::create_connection(&request).await.expect("connect failed");
-        let response = HttpClient::request(&mut stream, &request).await.expect("request failed");
-        let response_body = String::from_utf8(response.body().clone()).expect("failed to parse response body");
-        log::info!("response = {response:?}");
-        log::info!("response_body = {response_body}");
-    })
+    // Build the GET request
+    let request = Request::builder()
+        .method("GET")
+        .uri(uri)
+        .header("User-Agent", "http_client/1.0")
+        .header("Host", "www.google.com")
+        .body(vec![])
+        .expect("Failed to build request");
+
+    // Get the response
+    let mut stream = HttpClient::create_connection(&request).await.expect("connect failed");
+    let response = HttpClient::request(&mut stream, &request).await.expect("request failed");
+    let response_body = String::from_utf8(response.body().clone()).expect("failed to parse response body");
+    log::info!("response = {response:?}");
+    log::info!("response_body = {response_body}");
+}
+
+fn main() -> SimpleResult<()> {
+    Arc::<Executor>::with_main(|ex| smol::block_on(async_main(ex.clone())))
 }
